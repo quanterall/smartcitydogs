305211aa9a5530637cff7a8ec90a332f8cd09f55
vqcheslav-kara <39381632+vqcheslav-kara@users.noreply.github.com>
2018-05-18 11:20:19 +0300

    Update 20180517190256_create_users_types_table.exs

--- priv/repo/migrations/20180517190256_create_users_types_table.exs
+++ priv/repo/migrations/20180517190256_create_users_types_table.exs
@@ -3,7 +3,6 @@ defmodule Smartcitydogs.Repo.Migrations.CreateUsersTable do
 
   def change do
     create table("users_types") do
-      add :title, :text
       add :name, :text
       add :deleted_at, :naive_datetime
       timestamps()
