3d6ac2ccaa695e21b14bcd7bfe2cc9c1f0af4fc6
Hristislav <hristislav_bg@abv.bg>
2018-05-18 14:48:58 +0300

    [GH-#1] Migrations: Table signals and its relations(category, type)

---
+++ priv/repo/migrations/20180518113913_create_signals_category_relation.exs
@@ -0,0 +1,9 @@
+defmodule Smartcitydogs.Repo.Migrations.CreateSignalsCategoryRelation do
+  use Ecto.Migration
+
+  def change do
+    alter table(:signals) do
+      add :signals_categories_id, references(:signals_categories)
+    end
+  end
+end
