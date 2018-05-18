cf3b854bfb0dbce7ecd4480a63ad26d4abba12e7
pavel <pavel.kanchev@quanterall.com>
2018-05-18 12:33:38 +0300

    [GH-#2] Add migration

---
+++ priv/repo/migrations/20180518083906_create_header_slides_table.exs
@@ -0,0 +1,13 @@
+defmodule Smartcitydogs.Repo.Migrations.CreateHeaderSlidesTable do
+  use Ecto.Migration
+
+  def change do
+    create table("header_slides") do      
+      
+      add :image_url, :text
+      add :text, :text
+            
+      timestamps()
+    end
+  end
+end
