21e7b55fad5899db9baf0bc6551e101dce0e2737
Hristislav <hristislav_bg@abv.bg>
2018-05-18 15:39:34 +0300

    [GH-#2]

--- config/dev.exs
+++ config/dev.exs
@@ -51,8 +51,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :smartcitydogs, Smartcitydogs.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "123456",
-  database: "smartcitydogs_dev",
-  hostname: "localhost",
-  pool_size: 10
+    username: System.get_env("DB_USER"),
+    password: System.get_env("DB_PASSWORD"),
+    database: System.get_env("DB_NAME"),
+    hostname: System.get_env("DB_HOST"),
+    pool_size: 10
