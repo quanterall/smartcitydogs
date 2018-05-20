044ce7c203619ff0544e7a8ecf4ff4303b46453f
Vyacheslav Kara <vqcheslav.kara@quanterall.com>
2018-05-20 17:49:08 +0300

    [GH-#17] Migration: tables users and users_types

--- config/dev.exs
+++ config/dev.exs
@@ -11,8 +11,14 @@ config :smartcitydogs, SmartcitydogsWeb.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: [node: ["node_modules/brunch/bin/brunch", "watch", "--stdin",
-                    cd: Path.expand("../assets", __DIR__)]]
+  watchers: [
+    node: [
+      "node_modules/brunch/bin/brunch",
+      "watch",
+      "--stdin",
+      cd: Path.expand("../assets", __DIR__)
+    ]
+  ]
 
 # ## SSL Support
 #
@@ -51,8 +57,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :smartcitydogs, Smartcitydogs.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "123456",
-  database: "smartcitydogs_dev",
-  hostname: "localhost",
+  username: System.get_env("DB_USER"),
+  password: System.get_env("DB_PASSWORD"),
+  database: System.get_env("DB_NAME_SMART_CITY_DOGS"),
+  hostname: System.get_env("DB_HOST"),
   pool_size: 10
---
+++ priv/repo/migrations/20180517190256_create_users_types_table.exs
@@ -0,0 +1,11 @@
+defmodule Smartcitydogs.Repo.Migrations.CreateUsersTable do
+  use Ecto.Migration
+
+  def up do
+    create table("users_types") do
+      timestamps
+      add(:deleted_at, :naive_datetime)
+      add(:name, :text)
+    end
+  end
+end
