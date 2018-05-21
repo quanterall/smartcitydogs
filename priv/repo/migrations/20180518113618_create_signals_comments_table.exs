ebdba066cfbba8646a599319b8c397e89b2b4d0a
Hristislav <hristislav_bg@abv.bg>
2018-05-21 09:17:33 +0300

    [GH-2] Timestamps fixed

--- priv/repo/migrations/20180518113618_create_signals_comments_table.exs
+++ priv/repo/migrations/20180518113618_create_signals_comments_table.exs
@@ -3,8 +3,8 @@ defmodule Smartcitydogs.Repo.Migrations.CreateSignalsCommentsTable do
   #add
   def change do
     create table("signals_comments") do
-      add :deleted_at, :naive_datetime
       add :comment, :text
+      add :deleted_at, :naive_datetime
       timestamps()
     end
   end
