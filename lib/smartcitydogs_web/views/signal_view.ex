a74fcd6b4b6126c850b098f052e22a353f41c779
Hristislav <hristislav_bg@abv.bg>
2018-06-08 10:24:27 +0300

    [GH-#35] View counter is working as it should be wheter in specific signal or not

--- lib/smartcitydogs_web/views/signal_view.ex
+++ lib/smartcitydogs_web/views/signal_view.ex
@@ -12,22 +12,24 @@ defmodule SmartcitydogsWeb.SignalView do
 
   end
 
-  
+
 
   def get_signals_view_count(signal_id) do
     list = Smartcitydogs.DataSignals.get_signal_support_count(signal_id)
-    IO.inspect(list)
-    IO.puts "----------------------------"
+
     if list != [] do
-      #IO.inspect(list)
       [head | tail] = list
-      IO.inspect(head)
-      #count = head.count
-      #IO.inspect(count)
       count = head.view_count
       IO.inspect(count)
       IO.puts "---------------------------------------"
       Smartcitydogs.DataSignals.update_signal(head,%{view_count: count+1})
+      views = head.view_count + 1
+      IO.puts "________________________VIEWS________________________"
+      IO.inspect(views)
+      IO.puts "_________________________________________________"
+
     end
+    IO.inspect(views)
+    views
   end
 end
