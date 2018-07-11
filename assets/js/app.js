7e48397a04f2ad087f59cfd3b1d5acf3c0ee20a3
Hristislav <hristislav_bg@abv.bg>
2018-07-11 14:34:19 +0300

    [GH-#82] fixed scrolling bar after closing the form

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,21 +22,6 @@ import $ from "jquery";
 
 ////////////ORIGINAL//////////////
 
-// console.log("test");
-// $("#like").click(function(){
-//
-//   $.ajax({
-//     method: "GET",
-//     url: "/signals/something_else",
-//     data: {"show-count": $("#signal-id").text()}
-//   }).then(function(data) {
-//     $("#signal-id").text(data.new_count);
-//   })
-// });
-
-//console.log("test");
-//var a=$(".btnSignal");
-//console.log(a);
 $("#like").click(function() {
 
     $.ajax({
@@ -68,11 +53,7 @@ $("#comment").click(function() {
 });
 
 $(function() {
-  // $('.send-signal-container').hide();
-  //
-  // $('#send-signal').on('click', function() {
-  //   $('.send-signal-container').load('signals/new');
-  // })
+
 
   //-----------------all signals----------------
   $('#all-signals').on('click', function() {
@@ -107,29 +88,24 @@ $(function(){
   $('#send-signal').on('click', function(){
     // $('.send-signal-contaier').load('signals/new');
     $('main').css("position","fixed");
+    $('main').css("width","100%");
     $('.modal-content-signal').load('signals/new');
-    // console.log("ala bala");
-    // console.log(modal);
-    // console.log(span);
+
 
     modal.style.display = "block";
   });
   span.onclick = function() {
       modal.style.display = "none";
 
-      setTimeout(function() {
-         //  location.reload()
+      $('main').css("position","initial");
 
-          location.reload();
-      }, 0);
   }
 
 
 });
 
 $(function(){
-  // var modal = document.getElementById('myModal');
-  // var btn = document.getElementById("button-change-status");
+
   var modal = document.getElementById('myModal');
   var span = document.getElementsByClassName("close")[0];
   var id;
@@ -150,7 +126,7 @@ $(function(){
     }
   }
   $('#type-chosen').on('click',function(){
-    // var SelectedType = $('.select-type').find(":selected").text();
+
     SelectedType = $(".select-type option:selected").val()
     console.log(SelectedType);
     console.log("Buttton1: "+id);
@@ -164,85 +140,13 @@ $(function(){
         }
      })
 
-    // modal.style.display ="none";
+
 
      setTimeout(function() {
         //  location.reload()
          window.location.href = '/signals';
      }, 1000);
-//     var form = new FormData();
-// form.append("email", "sonyft@abv.bg");
-// form.append("password", "123456");
 
-// var settings = {
-//   "async": true,
-//   "crossDomain": true,
-//   "url": "http://localhost:4000/api/users/sign_in",
-//   "method": "POST",
-//   "headers": {
-//     "Cache-Control": "no-cache",
-//     "Postman-Token": "2a5b4054-16ab-4ec9-b532-e4ba555cc523"
-//   },
-//   "processData": false,
-//   "contentType": false,
-//   "mimeType": "multipart/form-data",
-//   "data": form
-// }
-
-// $.ajax(settings).done(function (response) {
-//   console.log(response);
-// });
-
-//     var settings = {
-//   "async": true,
-//   "crossDomain": true,
-//   "url": "http://localhost:4000/api/signals/"+id,
-//   "method": "PUT",
-//   "headers": {
-//     "Content-Type": "application/x-www-form-urlencoded",
-//     "Cache-Control": "no-cache",
-//     "Postman-Token": "901b1893-aa01-40df-8975-c3ac6299e525"
-//   },
-//   "data": {
-//     "signal[signals_types_id]": SelectedType
-//   }
-// }
-//     console.log(settings);
-// $.ajax(settings).done(function (response) {
-//   console.log("Response " + response);
-// });
-
-// var form1 = new FormData();
-// form1.append("id", "2");
-
-// var settings = {
-//   "async": true,
-//   "crossDomain": true,
-//   "url": "http://localhost:4000/api/users/logout",
-//   "method": "POST",
-//   "headers": {
-//     "Cache-Control": "no-cache",
-//     "Postman-Token": "ab1b1027-6704-4157-9f56-2889b45ec5dc"
-//   },
-//   "processData": false,
-//   "contentType": false,
-//   "mimeType": "multipart/form-data",
-//   "data": form
-// }
-
-// $.ajax(settings).done(function (response) {
-//   console.log(response);
-// });
 
   });
 });
-
-// $(function(){
-//   $('#type-chosen').on('click',function(){
-//     // var SelectedType = $('.select-type').find(":selected").text();
-//     var SelectedType = $(".select-type option:selected").val()
-//     console.log(SelectedType);
-//     console.log("Buttton1: "+id);
-//
-//   });
-// });
