87fb46c71d62a41ed960b30771a869cf49f6fd7e
pavel <pavel.kanchev@quanterall.com>
2018-07-26 11:23:52 +0300

    [GH-#103] Redirect to Home Page

--- lib/smartcitydogs_web/auth/current_user.ex
+++ lib/smartcitydogs_web/auth/current_user.ex
@@ -7,8 +7,6 @@ defmodule Smartcitydogs.CurrentUser do
   def call(conn, _opts) do
     current_user = current_resource(conn)
     assign(conn, :current_user, current_user)
-    # if(current_user == nil){
-    #   redirect(conn, to: session_path(Endpoint, :login))
-    # }
+    
   end
 end
