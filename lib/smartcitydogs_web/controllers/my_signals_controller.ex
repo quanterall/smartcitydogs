2f662ed00e0946dab3ee05203cdf2cc859d2fa9d
Hristislav <hristislav_bg@abv.bg>
2018-08-22 11:58:11 +0300

    #146 - Made the design and responsiveness over the signals of the current user. Changed some css because the boxes were with the same size compared to the home page. They need to be smaller. Added link to the navigation bar item (Hello, name) as it refers to the user information page.

--- lib/smartcitydogs_web/controllers/my_signals_controller.ex
+++ lib/smartcitydogs_web/controllers/my_signals_controller.ex
@@ -2,9 +2,19 @@ defmodule SmartcitydogsWeb.MySignalsController do
   use SmartcitydogsWeb, :controller
   alias Smartcitydogs.DataSignals
 
-  def index(conn, _params) do
+  def index(conn, params) do
+
+    if params == %{} do
+      x = 1
+    else
+      x = String.to_integer(params["page"])
+    end
+
     user = conn.assigns.current_user.id
+    sorted_signals = DataSignals.sort_signal_by_id()
     signals = DataSignals.get_user_signal(user)
-    render(conn, "my_signals.html", signals: signals)
+    
+    page = Smartcitydogs.Repo.paginate(sorted_signals, page: x, page_size: 8)
+    render(conn, "my_signals.html", signals: signals, page: page)
   end
 end
