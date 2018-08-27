3204e086cbe9d95043635ef19de642c1ee335a00
pavel <pavel.kanchev@quanterall.com>
2018-08-27 15:46:58 +0300

    #135 Update User Controller and View

--- lib/smartcitydogs_web/controllers/api/users_type_controller.ex
+++ lib/smartcitydogs_web/controllers/api/users_type_controller.ex
@@ -11,34 +11,34 @@ defmodule SmartcitydogsWeb.UsersTypeControllerAPI do
     render(conn, "index.json", users_types: users_types)
   end
 
-  def create(conn, %{"users_type" => users_type_params}) do
-    with {:ok, %UsersType{} = users_type} <- DataUsers.create_user_type(users_type_params) do
-      conn
-      |> put_status(:created)
-      |> put_resp_header("location", users_type_controller_api_path(conn, :show, users_type))
-      |> render("show.json", users_type: users_type)
-    end
-  end
+  # def create(conn, %{"users_type" => users_type_params}) do
+  #   with {:ok, %UsersType{} = users_type} <- DataUsers.create_user_type(users_type_params) do
+  #     conn
+  #     |> put_status(:created)
+  #     |> put_resp_header("location", users_type_controller_api_path(conn, :show, users_type))
+  #     |> render("show.json", users_type: users_type)
+  #   end
+  # end
 
   def show(conn, %{"id" => id}) do
     users_type = DataUsers.get_user_type(id)
     render(conn, "show.json", users_type: users_type)
   end
 
-  def update(conn, %{"id" => id, "users_type" => users_type_params}) do
-    users_type = DataUsers.get_user_type(id)
+  # def update(conn, %{"id" => id, "users_type" => users_type_params}) do
+  #   users_type = DataUsers.get_user_type(id)
 
-    with {:ok, %UsersType{} = users_type} <-
-           DataUsers.update_users_type(users_type, users_type_params) do
-      render(conn, "show.json", users_type: users_type)
-    end
-  end
+  #   with {:ok, %UsersType{} = users_type} <-
+  #          DataUsers.update_users_type(users_type, users_type_params) do
+  #     render(conn, "show.json", users_type: users_type)
+  #   end
+  # end
 
-  def delete(conn, %{"id" => id}) do
-    users_type = DataUsers.get_user_type(id)
+  # def delete(conn, %{"id" => id}) do
+  #   users_type = DataUsers.get_user_type(id)
 
-    with {:ok, %UsersType{}} <- DataUsers.delete_user_type(users_type) do
-      send_resp(conn, :no_content, "")
-    end
-  end
+  #   with {:ok, %UsersType{}} <- DataUsers.delete_user_type(users_type) do
+  #     send_resp(conn, :no_content, "")
+  #   end
+  # end
 end
