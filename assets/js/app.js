b879e31e256af1e3db5ac68455e721620703ac00
pavel <pavel.kanchev@quanterall.com>
2018-09-07 12:01:35 +0300

    Add Maps on Signals

--- assets/js/app.js
+++ assets/js/app.js
@@ -335,3 +335,19 @@ $('.signal-gallery').slick({
 
 
 });
+
+$('#view_map').click(function() {    
+    var cor_a = $('#cor_a').text();
+    var cor_b = $('#cor_b').text();
+    var mymap = L.map('signal_map').setView([cor_a, cor_b], 16);
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+        maxZoom: 20,
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+        '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        id: 'mapbox.streets'
+    }).addTo(mymap);
+
+    L.marker([cor_a, cor_b]).addTo(mymap);
+});
