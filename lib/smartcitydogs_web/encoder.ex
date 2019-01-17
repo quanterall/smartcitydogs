9acee9c2d4f4b9b994c14706362ebb34285d57b1
Vyacheslav Kara <vyacheslav.kara@finatech.bg>
2019-01-17 08:21:17 +0200

    removed unused lines

--- lib/smartcitydogs_web/encoder.ex
+++ lib/smartcitydogs_web/encoder.ex
@@ -5,17 +5,15 @@ defmodule SmartcitydogsWeb.Encoder do
   end
 
   defp sanitize_recursive(map) do
-    sanitized =
-      map
-      |> Map.from_struct()
-      |> Map.drop([:__meta__, :__cardinality__, :__field__, :__owner__, :password])
-      |> Enum.map(fn
-        {key, %DateTime{} = value} when is_map(value) -> {key, DateTime.to_string(value)}
-        {key, value} when is_map(value) -> {key, sanitize_recursive(value)}
-        {key, value} when is_list(value) -> {key, encode(value)}
-        {key, value} when is_number(value) -> {key, value}
-        x -> x
-      end)
-      |> Map.new()
+    map
+    |> Map.from_struct()
+    |> Map.drop([:__meta__, :__cardinality__, :__field__, :__owner__, :password])
+    |> Enum.map(fn
+      {key, %DateTime{} = value} when is_map(value) -> {key, DateTime.to_string(value)}
+      {key, value} when is_map(value) -> {key, sanitize_recursive(value)}
+      {key, value} when is_list(value) -> {key, encode(value)}
+      x -> x
+    end)
+    |> Map.new()
   end
 end
